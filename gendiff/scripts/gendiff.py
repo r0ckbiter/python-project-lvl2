ed66bf54dc9c7b2ff44d2bd8f2d87ccd96dae0a3
Eduard Zhuravlev <insanerealty@gmail.com>
2022-04-02 21:39:42 +0200

    add help message

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file', metavar='first_file', type=str)
+    parser.add_argument('second_file', metavar='second_file', type=str)
+    args = parser.parse_args()
+    print(args)
+
+
+if __name__ == '__main__':
+    main()
