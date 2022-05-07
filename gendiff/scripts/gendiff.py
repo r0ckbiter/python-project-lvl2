8455877a1c76aa4c2f348c63847254b44c8ab736
Eduard Zhuravlev <insanerealty@gmail.com>
2022-05-07 16:18:14 +0200

    comparing json files (step3)

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 
 import argparse
+from gendiff import generate_diff
 
 
 def main():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file', metavar='first_file', type=str)
     parser.add_argument('second_file', metavar='second_file', type=str)
-    parser.add_argument('-f', '--format', metavar='FORMAT', help='set format of output')
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    print(args)
+    diff = generate_diff(args.first_file, args.second_file)
+    return diff
 
 
 if __name__ == '__main__':
