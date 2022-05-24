098e2b0aaafc9354a8135b3a72c33d79299dc17a
Eduard Zhuravlev <insanerealty@gmail.com>
2022-05-24 21:57:11 +0200

    'gendiff bug fixing'

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,7 +8,7 @@ def main():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file', metavar='first_file', type=str)
     parser.add_argument('second_file', metavar='second_file', type=str)
-    parser.add_argument('-f', '--format', help='set format of output')
+    parser.add_argument('-f', '--format', action='store', help='set format of output')
     args = parser.parse_args()
     print(generate_diff(args.first_file, args.second_file, args.format))
 
